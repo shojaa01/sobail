c3f863eb3fadcaed39c6fb2bac02d02c5c2626c0
shojaa <abolfazlshojai01@gmail.com>
2025-04-07 22:45:02 +0330

    create RES Map

--- Codeing/page/script.js
+++ Codeing/page/script.js
@@ -1,16 +1,3 @@
-const FullScreen = document.getElementById('FullScreen');
-
-function getFullscreenElement(){
-    return document.fullscreenElement
-    || document.webkitFullscreenElement
-    || document.mozFullscreenElement
-    || document.msFullscreenElement;
-}
-FullScreen.addEventListener("click" , () =>{
-    document.documentElement.requestFullscreen().catch((e) =>{
-        console.log(e)
-    })
-})
 
 
 // -------------------------------------------------------------------------
